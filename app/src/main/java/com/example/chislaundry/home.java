d6373e423783589c02998361dd883b57abeb79ce
Gusti Ayu Wahyu Whurapsari <94887500+gekarii07@users.noreply.github.com>
2023-06-21 22:25:37 +0800

    Menambahkan activity home, mengedit activity intro dan login

---
+++ app/src/main/java/com/example/chislaundry/home.java
@@ -0,0 +1,13 @@
+package com.example.chislaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
