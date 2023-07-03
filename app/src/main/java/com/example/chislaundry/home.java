29140f531d5fc2d624697199aeb7df71ee241d8c
Gusti Ayu Wahyu Whurapsari <94887500+gekarii07@users.noreply.github.com>
2023-07-03 23:02:33 +0800

    membuat login akun

--- app/src/main/java/com/example/chislaundry/home.java
+++ app/src/main/java/com/example/chislaundry/home.java
@@ -1,15 +1,17 @@
 package com.example.chislaundry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class home extends AppCompatActivity{
 
+    TextView tName;
     Button btndetail;
     ImageButton btnhome, btnriwayat, btnprofil, btnkeranjang, btncucisetrika, btnsetrika, btnpremiumwash, btndrycleaning;
 
@@ -18,6 +20,9 @@ public class home extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        tName = findViewById(R.id.name);
+        String name = getIntent().getStringExtra("name");
+        tName.setText(name);
         btndetail = findViewById(R.id.btnDetail);
         btnhome = findViewById(R.id.btnHome);
         btnriwayat = findViewById(R.id.btnRiwayat);
